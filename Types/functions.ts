2804a7d021a524e2f5c93f205b3f4663877d63fa
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2022-09-26 07:53:35 +0100

    finilize functions section

--- Types/functions.ts
+++ Types/functions.ts
@@ -20,7 +20,60 @@ function greet(person: string = 'stranger') {
   return `Good morning ${person}`
 }
 
-//* Fuction retun types
+//* Function retun types
+
+function devide(num: number): number {
+  return num / 2
+
+}
+
+// * Function return number or toString
+
+function random(num: number) {
+  if (Math.random() < 0.5) {
+    return num.toString()
+  }
+  return num
+
+}
+
+//* annonymous functions
+
+const colors = ['yellow', 'green', 'blue'];
+
+colors.map(color => {
+  color.toString()
+})
+
+
+
+//* Void functions that return  or at least  undefined or null
+
+
+function printTwice(msg: string): void {
+  console.log('hello')
+  console.log(msg)
+}
+
+
+
+//* Never, is function that return nothing
+
+function sendError(msg: string): never {
+  throw new Error(msg)
+}
+
+
+function gameLoop(name: string): never {
+  while (true) {
+    console.log(`still runing ${name}`)
+  }
+}
+
+
+printTwice('body')
+
+devide(100)
 
 
 
