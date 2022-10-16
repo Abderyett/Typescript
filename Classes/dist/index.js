95b32cf155d9698d4012b77aef7a7e406ac3ccca
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2022-10-16 07:30:20 +0100

    work with ts classes

--- Classes/dist/index.js
+++ Classes/dist/index.js
@@ -1,63 +1,16 @@
-class Player {
-	#score = 0;
-	numLives = 10;
-	constructor(first, last) {
-		this.first = first;
-		this.last = last;
-	}
-	taunt() {
-		console.log('boo!!');
-	}
-
-	getScore() {
-		return this.#score;
-	}
-
-	updateScore(newScore) {
-		this.#score = newScore;
-	}
-	loseLife() {
-		this.numLives -= 1;
-	}
-	static randomPlayer() {
-		return new Player('Andy', 'Sumberg');
-	}
-
-	get fullName() {
-		return `${this.first} ${this.last}`;
-	}
-	set score(newScore) {
-		if (newScore < 0) {
-			throw new Error('Score must be positive');
-		}
-		this.#score = newScore;
-	}
-}
-
-class AdminPlyer extends Player {
-	constructor(first, last, powers) {
-		super(first, last);
-		this.powers = powers;
-	}
-	isAdmin = true;
-}
-
-const admin = new AdminPlyer('Admin', 'Player');
-admin.powers = ['delete', 'edit'];
-
-// const player1 = new Player('colt', 'steele');
-// player1.taunt();
-// // console.log(player1.first);
-
-// const player2 = new Player('golden', 'brown');
-// player2.taunt();
-// console.log(player2.numLives);
-// player2.loseLife();
-// console.log(player2.numLives);
-
-// console.log(player1.getScore());
-// player1.updateScore(56);
-// player1.score = 123;
-// console.log(player1.getScore());
-
-// console.log(player2.fullName);
+"use strict";
+var Player = /** @class */ (function () {
+    function Player(name, age) {
+        this.score = 0;
+        this.name = name;
+        this.age = age;
+    }
+    Player.prototype.secretMethod = function () {
+        console.log('Secret method');
+    };
+    return Player;
+}());
+var player1 = new Player('Jesse', 23);
+// player1.score = 23;
+// it's still run method on runtime
+// player1.secretMethod();
--- Classes/index.ts
+++ Classes/index.ts
@@ -1,1 +1,21 @@
-console.log("it's runing");
+class Player {
+	public readonly name: string;
+	public age: number;
+	private score: number = 0;
+
+	constructor(name: string, age: number) {
+		this.name = name;
+		this.age = age;
+	}
+
+	private secretMethod(): void {
+		console.log('Secret method');
+	}
+}
+
+const player1 = new Player('Jesse', 23);
+
+// player1.score = 23;
+
+// it's still run method on runtime
+// player1.secretMethod();
