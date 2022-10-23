b81dc1fee4bf29884f127744fab56e8b2b6187e1
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2022-10-23 07:35:00 +0100

    start with generics

---
+++ Generics/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+var num = [];
+var colors = [];
+var input = document.querySelector('#username');
+input.value = 'Jesse James';
+var btn = document.querySelector('.btn');
---
+++ Generics/src/index.ts
@@ -0,0 +1,7 @@
+const num: Array<number> = [];
+const colors: Array<string> = [];
+
+const input = document.querySelector<HTMLInputElement>('#username')!;
+input.value = 'Jesse James';
+
+const btn = document.querySelector<HTMLButtonElement>('.btn'!)!;
