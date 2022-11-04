7bd4818b165cc962c5bda707df94e17d104309aa
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2022-11-04 11:31:19 +0100

    usestate with typescript

--- shopping-app/src/App.tsx
+++ shopping-app/src/App.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Greeter from './components/Greeter'
 import './App.css';
+import ShoppingList from './components/ShoppingList';
+import Item from './models/item'
 
 function App() {
+
+const [items, setItems] = useState<Item[]>([]);
+
+	// const items = [
+  //   {id:1,product:"Lemon",quantity:5},
+  //   {id:2,product:"Chicken",quantity:2}
+  // ];
 	return (
-		<div className='App'>
-			<Greeter person="Colt"/>
-			<Greeter person="Jesse"/>
-			<Greeter person="Andy"/>
+		<div>
+			{/* <Greeter person="Colt"/> */}
+		  <ShoppingList items={items}/>
 		</div>
 	);
 }
---
+++ shopping-app/src/components/ShoppingList.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import Item from '../models/item';
+
+
+
+
+interface ShoppingListProps {
+  items:Item[];
+}
+
+
+function ShoppingList({items}:ShoppingListProps) {
+	return (
+		<div>
+			<h1>Shopping List</h1>
+
+			<ul>
+			{items.map(item =>(
+        <li key={item.id}>{item.product} - Qty: {item.quantity}</li>
+      ))}
+     
+			</ul>
+		</div>
+	);
+}
+
+export default ShoppingList;
