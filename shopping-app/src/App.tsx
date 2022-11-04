3b60d7e77a295f39bf8bd2bf8b26653c1aa40541
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2022-11-04 18:31:18 +0100

    Finished The course

--- shopping-app/src/App.tsx
+++ shopping-app/src/App.tsx
@@ -4,12 +4,12 @@ import './App.css';
 import ShoppingList from './components/ShoppingList';
 import Item from './models/item'
 import ShoppingListForm from './components/ShoppingListform';
-
+import { v4 } from 'uuid';
 function App() {
 
 const [items, setItems] = useState<Item[]>([]);
-const addItem =(product:string)=>{
-	console.log(product);
+const addItem =(product:string,quantity:number)=>{
+	setItems([...items,{id:v4(),product,quantity}]);
 }
 
 	// const items = [
--- shopping-app/src/components/ShoppingListform.tsx
+++ shopping-app/src/components/ShoppingListform.tsx
@@ -2,22 +2,26 @@ import React,{useRef} from 'react';
 
 
 interface ShoppingListForm {
-  onAddItem(item: string): void;
+  onAddItem(item: string,quantity: number): void;
 }
 function ShoppingListForm({onAddItem}:ShoppingListForm) {
 
-const inputRef=useRef<HTMLInputElement>(null)
+const productInput=useRef<HTMLInputElement>(null)
+const quantityInput=useRef<HTMLInputElement>(null)
 
   function handleSubmit(e:React.FormEvent) { 
     e.preventDefault();
-   const newProduct=inputRef.current!.value
-   onAddItem(newProduct)
-   inputRef.current!.value=""
+   const newProduct=productInput.current!.value
+   const newQty=parseInt(quantityInput.current!.value)
+   onAddItem(newProduct,newQty);
+   productInput.current!.value=""
+   quantityInput.current!.value=""
     
   }
   return ( 
     <form onSubmit={handleSubmit}>
-      <input type="text" placeholder="Product Name" ref={inputRef}/>
+      <input type="text" placeholder="Product Name" ref={productInput}/>
+      <input type="number" min={0}  ref={quantityInput}/>
       <button type="submit">Add item</button>
     </form>
    );
